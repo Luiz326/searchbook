a9093265dc34e4e64c73e38b0dd755062cf563e2
Luiz Mitsuru Dai <61781452+Luiz326@users.noreply.github.com>
2021-12-14 00:57:31 -0300

    css atualizado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { Component } from "react";
 import BuscaLivros from "./components/BuscaLivros";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 
 
 class App extends Component {
   render(){
  return (
     <div >
+      <Header/>
       <BuscaLivros />
+      <Footer/>
     </div>
   );
   }
---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+export default function Header(){
+    return(
+        <header>
+           <h1>SearchBook</h1> 
+        </header>
+    )
+}
