97bcd7cd65ee693a8093e128963ea9a822273658
Luiz Mitsuru Dai <61781452+Luiz326@users.noreply.github.com>
2021-12-13 22:39:51 -0300

    Atualizado

--- src/components/BuscaLivros/index.js
+++ src/components/BuscaLivros/index.js
@@ -10,30 +10,33 @@ export default function BuscaLivros() {
     
     const [livros, setLivros] =useState([]);
     const [busca, setBusca] = useState('');
-
-    const lowerBusca = busca.toLowerCase();
-
-    const livrosFiltrados = livros
-    .filter((livro)=> livro.toString(livro).toLowerCase().includes(lowerBusca));
-
-   console.log(busca)
-
+     
     useEffect(()=>{
-        api.get( " https://hn.algolia.com/api/v1/search?query=").then(({data})=>{
+       function pegaLivro(){
+             api.get( "api/v1/search?query=").then(({data})=>{
             setLivros(data.hits);
         })
-        console.log(livros)
-       // eslint-disable-next-line
+        
+        }
+       
+       pegaLivro()
+       
+        // eslint-disable-next-line
     },[busca]);
     
- 
+  async  function pegaLivroSearch(){
+      await  api.get( "api/v1/search?query=" + busca).then(({data})=>{
+       setLivros(data.hits);
+   })
+   
+   }
    
     return (
         <div className="container">
             <h1>Busca Livros</h1>
-            <input type="text" value={busca} onChange={(ev)=>setBusca(ev.target.value)} /><span><MdSearch fontSize="30px" /></span>
+            <input type="text" value={busca} onChange={(ev)=>setBusca(ev.target.value)} /><span onClick={pegaLivroSearch}><MdSearch fontSize="30px" /></span>
            
-               {livrosFiltrados?.map((livro)=>(
+               {livros?.map((livro)=>(
                    <div key={livro.parent_id} className="container-books">
 
                    <span className="p-span-autor"><BiUser fontSize="20px" /></span>  <p> {livro.author}</p>
